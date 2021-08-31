54cc93dc87965776559ebd1f9d8b5f45ceb06b77
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-08-31 22:39:24 +0200

    base structure

---
+++ src/main/scala/com/vanalex/SparkSessionWrapper.scala
@@ -0,0 +1,11 @@
+package com.vanalex
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionWrapper extends Serializable {
+
+  lazy val spark: SparkSession = {
+    SparkSession.builder().master("local").appName("spark session").getOrCreate()
+  }
+
+}
