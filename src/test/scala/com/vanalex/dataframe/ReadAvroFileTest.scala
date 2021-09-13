fe8a28dd6d033f72b36ccc810e23a604c1d1c594
vanalex <apjorquera@gmail.com>
2021-09-13 21:18:54 +0200

    read avro files examples

---
+++ src/test/scala/com/vanalex/dataframe/ReadAvroFileTest.scala
@@ -0,0 +1,20 @@
+package com.vanalex.dataframe
+
+import com.vanalex.SparkSessionTestWrapper
+import org.junit.Assert.assertNotNull
+import org.scalatest.FunSpec
+
+class ReadAvroFileTest extends FunSpec with SparkSessionTestWrapper{
+
+  describe("loads avro dataframe") {
+
+    it("it loads dataframe given avro path") {
+      val pathFile = "src/main/resources/flight/avro/*"
+
+      val dataframe = spark.read.format("avro").option("path", pathFile).load()
+      assertNotNull(dataframe)
+      val schema = dataframe.encoder.schema
+      println(schema)
+    }
+  }
+}
